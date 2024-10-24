98b9c26f5b65c452a89373687e6f2ecae0c61981
Rodrigo Pavez <pavez.rodrigo@gmail.com>
2024-10-23 22:21:46 -0300

    upd axios

--- axiosConfig.js
+++ axiosConfig.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-// Crear una instancia de Axios con la configuración de la URL base
 const instance = axios.create({
-  baseURL: 'https://backendgaleria.onrender.com/api', // URL base de tu backend en Render
+  baseURL: 'https://backendgaleria.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
