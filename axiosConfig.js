b0ce6256c705e5d99d35811c36269407bcd8cca7
Rodrigo Pavez <pavez.rodrigo@gmail.com>
2024-10-24 18:01:14 -0300

    upd axios

--- axiosConfig.js
+++ axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://backendgaleria.onrender.com/',
+  baseURL: 'https://backendgaleria.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
