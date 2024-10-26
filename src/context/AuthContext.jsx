f588c30b84a1ed3f96392541ba1f86d2c4067026
Rodrigo Pavez <pavez.rodrigo@gmail.com>
2024-10-26 16:50:06 -0300

    upd authcon

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState, useEffect } from 'react';
-import axios from '../axiosConfig';
+import axios from '../../axiosConfig';
 
 export const AuthContext = createContext();
 
