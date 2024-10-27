b2d9e1d8d428b2bada3f625021febc833e3c2a51
Rodrigo Pavez <pavez.rodrigo@gmail.com>
2024-10-27 00:14:14 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import '../App.css';
 
 function Footer() {
   return (
-    <footer className="bg-dark text-center text-lg-start navbar-expang-lg">
+    <footer className="bg-dark text-center text-lg-start container-fluid">
       <div className="text-center p-3 text-light">
         © 2024 Galería de Arte: 
         <a className="text-light" href="/"> Legales </a> | 
