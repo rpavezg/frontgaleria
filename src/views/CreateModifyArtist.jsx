97c0fbbd4eb696752bc38d93464ef9a82abdeae2
Rodrigo Pavez <pavez.rodrigo@gmail.com>
2024-10-27 00:40:28 -0300

    upd crmod

--- src/views/CreateModifyArtist.jsx
+++ src/views/CreateModifyArtist.jsx
@@ -82,7 +82,7 @@ const CreateModifyArtist = () => {
   };
 
   return (
-    <div className="container">
+    <div className="container mt-">
       <h2>{selectedArtist ? 'Modificar Artista' : 'Crear Artista'}</h2>
       <select className="form-select mb-3" onChange={handleSelectChange}>
         <option value="">Seleccionar Artista para Modificar</option>
